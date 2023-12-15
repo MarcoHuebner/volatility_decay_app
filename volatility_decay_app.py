0aa60facb05ab7e1963db8e6551af2f50444915e
MarcoHuebner <57489799+MarcoHuebner@users.noreply.github.com>
2023-12-15 22:24:30 +0100

    Update volatility_decay_app.py

--- volatility_decay_app.py
+++ volatility_decay_app.py
@@ -18,6 +18,8 @@ describing the quadratic behaviour:
 import numpy as np
 import plotly.graph_objects as go
 
+import streamlit as st
+
 import dash
 from dash import dcc
 from dash import html
@@ -91,173 +93,20 @@ def leveraged_return_mesh(
     return mesh * 100
 
 
-# initialize the app
-app = dash.Dash(__name__)
+# define display functions
+def update_result(a, b, c):
+    # calculate the Kelly Criterion
+    # NOTE: the factor of 100 corrects for the percentage values
+    # NOTE: the factor of 252 corrects for the daily values
+    return f"#### Kelly Fraction f: {100 * (a - b) / c**2:.2f}"
+
 # define parameters (except for leverage all in percent)
-lev_r = 2
+lev_r = 2.0
 exp_r = 0.6
 libor = 0.5
 # define heatmap marginals (-50% - 50% underlying CAGR, 0-100% annualized volatility)
 cagr_underlying = np.linspace(-50, 50, 200)
 volatility_undr = np.linspace(0.0, 100, 100)
-
-# define the layout
-app.layout = html.Div(
-    [
-        # define the header
-        html.Header(
-            children=[
-                html.H1(
-                    children="Volatility Decay & Kelly Criterion",
-                    style={
-                        "textAlign": "center",
-                        "fontSize": "40px",
-                        "color": "#ffffff",
-                    },
-                ),
-                html.H3(
-                    children="Volatility decay, the phenomenon of underperforming the market "
-                    + "despite having a larger position, is a central concept in the realm "
-                    + "of financial markets and risk management. Navigating this environment "
-                    + "requires strategic and rational decision-making when it comes to "
-                    + "position sizing, and the Kelly Criterion, developed by John L. Kelly Jr, "
-                    + "is proving to be a valuable tool. This formula allows investors to optimize "
-                    + "the size of their positions while balancing long-term growth expectations with "
-                    + "risk mitigation. By incorporating probabilities of success and risk/return "
-                    + "ratios, the Kelly Criterion provides a smart strategy for investors looking "
-                    + "to make informed decisions in the stock market.",
-                    style={
-                        "textAlign": "center",
-                        "width": "70vw",
-                        "fontSize": "20px",
-                        "color": "#ffffff",
-                    },
-                ),
-            ],
-            style={
-                "display": "flex",
-                "flexDirection": "column",
-                "justifyContent": "center",
-                "alignItems": "center",
-                "textAlign": "center",
-                "height": "35vh",
-                "margin": "0",
-                "fontSize": "40px",
-                "color": "#ffffff",
-                "background": "linear-gradient(to right, #67001f, #053061)",
-            },
-        ),
-        # TODO: Make auto-scrolling directly go all the way to the bottom
-        html.Div(id="page-content"),
-        # add a description
-        # TODO: Add a more detailed description and Kelly Plot (conservative -> danger)
-        html.Div(
-            [
-                html.Span(
-                    "Calculating the ",
-                    style={"fontSize": "1.5em", "fontWeight": "bold"},
-                ),
-                html.A(
-                    "Kelly Criterion",
-                    href="https://rhsfinancial.com/2017/06/20/line-aggressive-crazy-leverage/",
-                    style={"fontSize": "1.5em", "fontWeight": "bold"},
-                    target="_blank",
-                ),
-            ],
-            style={"textAlign": "center", "margin": "5vh auto auto auto"},
-        ),
-        # add sliders to calculate the Kelly Criterion
-        html.Div(
-            [
-                html.Label(
-                    "Yearly Expected Return [%]",
-                    style={"fontSize": "20px", "marginRight": "1vw"},
-                ),
-                dcc.Input(id="yearly_er", type="number", value=0.037 * 252),
-            ],
-            style={"textAlign": "center", "margin": "1vh auto 1vh auto"},
-        ),
-        html.Div(
-            [
-                html.Label(
-                    "Yearly Risk Free Rate",
-                    style={"fontSize": "20px", "marginRight": "1vw"},
-                ),
-                dcc.Input(id="yearly_risk_free", type="number", value=0.005 * 252),
-            ],
-            style={"textAlign": "center", "margin": "1vh auto 1vh auto"},
-        ),
-        html.Div(
-            [
-                html.Label(
-                    "Yearly Return Volatility [%]",
-                    style={"fontSize": "20px", "marginRight": "1vw"},
-                ),
-                dcc.Input(
-                    id="yearly_volatility",
-                    type="number",
-                    value=round(1.2 * np.sqrt(252), 2),
-                ),
-            ],
-            style={"textAlign": "center", "margin": "1vh auto 1vh auto"},
-        ),
-        html.Div(
-            id="result",
-            style={
-                "textAlign": "center",
-                "fontSize": "20px",
-                "margin": "5vh auto auto auto",
-            },
-        ),
-        # add a description
-        html.H2(
-            "Visualizing the Volatility Decay",
-            style={"textAlign": "center", "margin": "5vh auto auto auto"},
-        ),
-        # add dropdown for the contour/ heatmap
-        dcc.Dropdown(
-            id="data-source",
-            options=[
-                {"label": "Plot Style: Heatmap", "value": "heatmap"},
-                {"label": "Plot Style: Contour", "value": "contour"},
-            ],
-            value="heatmap",
-            style={
-                "textAlign": "center",
-                "width": "90vw",
-                "margin": "2vh auto auto auto",
-            },
-        ),
-        # add sliders with headings for the parameters
-        # TODO: Add a self-explaining description for each slider
-        html.Div(
-            [
-                html.Label("Leverage", style={"fontSize": "20px"}),
-                dcc.Slider(id="leverage-slider", min=0, max=10, step=0.5, value=lev_r),
-            ],
-            style={"textAlign": "center", "margin": "1vh auto 1vh auto"},
-        ),
-        html.Div(
-            [
-                html.Label("TER", style={"fontSize": "20px"}),
-                dcc.Slider(id="TER-slider", min=0, max=1, step=0.05, value=exp_r),
-            ],
-            style={"textAlign": "center", "margin": "1vh auto 1vh auto"},
-        ),
-        html.Div(
-            [
-                html.Label("LIBOR", style={"fontSize": "20px"}),
-                dcc.Slider(id="LIBOR-slider", min=0, max=3, step=0.25, value=libor),
-            ],
-            style={"textAlign": "center", "margin": "1vh auto 1vh auto"},
-        ),
-        # add the graph
-        dcc.Graph(
-            id="3d-plot",
-            style={"width": "90vw", "height": "65vh", "margin": "auto"},
-        ),
-    ]
-)
 
 # define the data
 data = [
@@ -285,33 +134,6 @@ data_contour = [
     )
 ]
 
-
-# define the callback for the calculation
-@app.callback(
-    Output("result", "children"),
-    [
-        Input("yearly_er", "value"),
-        Input("yearly_risk_free", "value"),
-        Input("yearly_volatility", "value"),
-    ],
-)
-def update_result(a, b, c):
-    # calculate the Kelly Criterion
-    # NOTE: the factor of 100 corrects for the percentage values
-    # NOTE: the factor of 252 corrects for the daily values
-    return f"Kelly Fraction f: {100 * (a - b) / c**2:.2f}"
-
-
-# define the callback for the plot
-@app.callback(
-    Output("3d-plot", "figure"),
-    [
-        Input("data-source", "value"),
-        Input("leverage-slider", "value"),
-        Input("TER-slider", "value"),
-        Input("LIBOR-slider", "value"),
-    ],
-)
 def update_plot(data_source, leverage, TER, LIBOR):
     # update data
     if data_source == "heatmap":
@@ -325,7 +147,7 @@ def update_plot(data_source, leverage, TER, LIBOR):
     # update layout
     fig.update_layout(
         title="Visualized Gain over Unleveraged ETF",
-        title_x=0.5,  # Center the title
+        title_x=0.35,  # Center the title
         title_font=dict(size=24),
         xaxis_title="CAGR Underlying [%]",
         yaxis_title="Volatility [%]",
@@ -333,6 +155,61 @@ def update_plot(data_source, leverage, TER, LIBOR):
 
     return fig
 
-
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    # define the layout
+    st.set_page_config(layout="wide")
+    st.markdown(
+        """
+        <style>
+        .header {
+            background: linear-gradient(to right, #67001f, #053061);
+            padding: 2em;
+            color: white;
+            max-width: 100%;
+            text-align: center;
+        }
+        </style>
+
+        <div class="header">
+            <h1>Volatility Decay & Kelly Criterion</h1>
+            <h3>Volatility decay, the phenomenon of underperforming the market 
+            despite having a larger position, is a central concept in the realm 
+            of financial markets and risk management. Navigating this environment 
+            requires strategic and rational decision-making when it comes to 
+            position sizing, and the Kelly Criterion, developed by John L. Kelly Jr,
+            is proving to be a valuable tool. This formula allows investors to optimize
+            the size of their positions while balancing long-term growth expectations with
+            risk mitigation. By incorporating probabilities of success and risk/return
+            ratios, the Kelly Criterion provides a smart strategy for investors looking
+            to make informed decisions in the stock market.</h3>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    # Header for the Kelly Criterion
+    st.markdown("", unsafe_allow_html=True)
+    st.write("## Kelly Criterion Calculator")
+    # Text input for yearly expected return
+    yearly_er = st.number_input("Yearly Expected Return [%]", value=0.037 * 252)
+    # Text input for yearly risk free rate
+    yearly_risk_free = st.number_input("Yearly Risk Free Rate", value=0.005 * 252)
+    # Text input for yearly return volatility
+    yearly_volatility = st.number_input("Yearly Return Volatility [%]", value=1.2 * np.sqrt(252))
+    # Display the result
+    result = update_result(yearly_er, yearly_risk_free, yearly_volatility)
+    st.write(result)
+
+    # Header for the plot
+    st.markdown("", unsafe_allow_html=True)
+    st.write("## Visualize the Gain over Unleveraged ETF")
+    # Dropdown for the plot style
+    data_source = st.selectbox("Plot Style", ["Heatmap", "Contour"], index=0)
+    # Slider for leverage
+    leverage = st.slider("Leverage", min_value=0.0, max_value=10.0, value=lev_r, step=0.5)
+    # Slider for TER
+    ter = st.slider("TER", min_value=0.0, max_value=1.0, value=exp_r, step=0.05)
+    # Slider for LIBOR
+    libor = st.slider("LIBOR", min_value=0.0, max_value=3.0, value=libor, step=0.25)
+    # Placeholder for the graph
+    st.plotly_chart(update_plot(data_source, leverage, ter, libor), use_container_width=True)
